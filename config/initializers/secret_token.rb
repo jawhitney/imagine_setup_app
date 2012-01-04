d9bce4f5d4439a310a58a22ec0f6c9bf065d6574
Jonathan Whitney <jonathan.whitney@keynote.org>
2012-01-04 14:39:51 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ImagineSetupApp::Application.config.secret_token = 'ddd6ec78d39ae396b410898d3a72e13fe007c40e1d9f7bf21ae4d120069da56fcdcf052285bee188f259e2f4729bd18fb916ccf5777f335d365e0232ad68910e'
